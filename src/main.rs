beab19f90ea02405ed1939183ff59c76297518ac
esbrinachain <ebrinachain@email.com>
2026-01-20 17:10:09 +0100

    c1

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,15 @@ enum LinkedList {
     Empty,
     Node { value: i32, next: Box<LinkedList> },
 }
-
+impl LinkedList {
+    fn get(&self, index: usize) -> Option<i32> {
+        match (self, index) {
+            (LinkedList::Empty, _) => None,
+            (LinkedList::Node { value, .. }, 0) => Some(*value),
+            (LinkedList::Node { next, .. }, i) => next.get(i - 1),
+        }
+    }
+}
 fn main() {
     let list = LinkedList::Node {
         value: 1,
@@ -24,5 +32,8 @@ fn main() {
         next: Box::new(list),
     };
 
-    println!("{list:#?}");
+    println!("{:#?}", list);
+    if let Some(value) = list.get(2) {
+        println!("Tercer elemento: {}", value);
+    }
 }
